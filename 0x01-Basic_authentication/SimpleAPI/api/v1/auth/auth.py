5ffec819e1cadf14f1405512857956ac335c80f7
harper71 <princeazuka99@gmail.com>
2024-11-12 20:38:58 +0100

    commit changes

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -9,7 +9,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """required authentication"""
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> Union[str, None]:
         """header autorization"""
